ab8d73e9bfa81b6c40da839357e639564c538061
martynas.sebestinas <martynas@infotrans.lt>
2017-12-18 14:14:06 +0200

    pakeitimai

--- pong.js
+++ pong.js
@@ -1,6 +1,34 @@
-/**
- * Created by Vartotojas on 12/17/2017.
- */
+//Template Method
+class GameMechanism {
+    constructor(){
+        this.game = new Game();
+        this.MainLoop();
+    }
+    MainLoop(){
+        this.game.update();
+        this.game.draw();
+        setTimeout(this.MainLoop.bind(this), 33.3333);
+    }
+}
+
+class GameMechanismSpeedX2 extends GameMechanism{
+    constructor(){super();}
+    MainLoop(){
+        this.game.update();
+        this.game.draw();
+        setTimeout(this.MainLoop.bind(this), 15.3333);
+    }
+}
+
+class GameMechanismSpeedX3 extends GameMechanism{
+    constructor(){super();}
+    MainLoop(){
+        this.game.update();
+        this.game.draw();
+        setTimeout(this.MainLoop.bind(this), 10.3333);
+    }
+}
+
 class Game {
     constructor() {
         var canvas = document.getElementById("game");
@@ -9,73 +37,73 @@ class Game {
         this.context = canvas.getContext("2d");
         this.context.fillStyle = "white";
         this.keys = new KeyListener();
-        this.p1 = new Paddle(5, 0);
-        this.p1.y = this.height/2 - this.p1.height/2;
+        this.paddle1 = new Paddle(5, 0);
+        this.paddle1.y = this.height/2 - this.paddle1.height/2;
         this.display1 = new Display(this.width/4, 25);
-        this.p2 = new Paddle(this.width - 5 - 2, 0);
-        this.p2.y = this.height/2 - this.p2.height/2;
+        this.paddle2 = new Paddle(this.width - 7, 0);
+        this.paddle2.y = this.height/2 - this.paddle2.height/2;
         this.display2 = new Display(this.width*3/4, 25);
         this.ball = new Ball();
         this.ball.x = this.width/2;
         this.ball.y = this.height/2;
         this.ball.vy = Math.floor(Math.random()*12 - 6);
         this.ball.vx = 7 - Math.abs(this.ball.vy);
-
-        this.MainLoop();
+        this.abstractdraw = new AbstractDraw();
+        this.drawing = new Draw();
+        this.DrawingBallOrPaddle = new DrawBallOrPadle();
+        this.DrawingDisplay = new DrawDisplay();
     }
 
     draw() {
-        this.context.clearRect(0, 0, this.width, this.height);
-        this.context.fillRect(this.width/2, 0, 2, this.height);
+        this.drawing.clear(this.context, 0, 0, this.width, this.height);
+        this.drawing.fill(this.context, this.width/2, 0, 2, this.height);
 
-        this.p1.draw(this.context);
-        this.p2.draw(this.context);
+        this.DrawingBallOrPaddle.fill(this.context, this.paddle1.x, this.paddle1.y, this.paddle1.width, this.paddle1.height);
+        this.DrawingBallOrPaddle.fill(this.context, this.paddle2.x, this.paddle2.y, this.paddle2.width, this.paddle2.height);
+        this.DrawingBallOrPaddle.fill(this.context, this.ball.x, this.ball.y, this.ball.width, this.ball.height);
 
-        this.ball.draw(this.context);
-
-        this.display1.draw(this.context);
-        this.display2.draw(this.context);
+        this.DrawingDisplay.fill(this.context, this.display1.value, this.display1.x, this.display1.y);
+        this.DrawingDisplay.fill(this.context, this.display2.value, this.display2.x, this.display2.y);
     }
+
     update(){
-        if (this.paused)
-            return;
-        // To which Y direction the paddle is moving
         this.ball.update();
-        this.display1.value = this.p1.score;
-        this.display2.value = this.p2.score;
+        this.display1.value = this.paddle1.score;
+        this.display2.value = this.paddle2.score;
+
         if (this.keys.isPressed(83)) { // DOWN
-            this.p1.y = Math.min(this.height - this.p1.height, this.p1.y + 4);
+            this.paddle1.y = Math.min(this.height - this.paddle1.height, this.paddle1.y + 4);
         } else if (this.keys.isPressed(87)) { // UP
-            this.p1.y = Math.max(0, this.p1.y - 4);
+            this.paddle1.y = Math.max(0, this.paddle1.y - 4);
         }
 
         if (this.keys.isPressed(40)) { // DOWN
-            this.p2.y = Math.min(this.height - this.p2.height, this.p2.y + 4);
+            this.paddle2.y = Math.min(this.height - this.paddle2.height, this.paddle2.y + 4);
         } else if (this.keys.isPressed(38)) { // UP
-            this.p2.y = Math.max(0, this.p2.y - 4);
+            this.paddle2.y = Math.max(0, this.paddle2.y - 4);
         }
 
         if (this.ball.vx > 0) {
-            if (this.p2.x <= this.ball.x + this.ball.width &&
-                this.p2.x > this.ball.x - this.ball.vx + this.ball.width) {
-                var collisionDiff = this.ball.x + this.ball.width - this.p2.x;
+            if (this.paddle2.x <= this.ball.x + this.ball.width &&
+                this.paddle2.x > this.ball.x - this.ball.vx + this.ball.width) {
+                var collisionDiff = this.ball.x + this.ball.width - this.paddle2.x;
                 var k = collisionDiff/this.ball.vx;
                 var y = this.ball.vy*k + (this.ball.y - this.ball.vy);
-                if (y >= this.p2.y && y + this.ball.height <= this.p2.y + this.p2.height) {
+                if (y >= this.paddle2.y && y + this.ball.height <= this.paddle2.y + this.paddle2.height) {
                     // collides with right paddle
-                    this.ball.x = this.p2.x - this.ball.width;
+                    this.ball.x = this.paddle2.x - this.ball.width;
                     this.ball.y = Math.floor(this.ball.y - this.ball.vy + this.ball.vy*k);
                     this.ball.vx = -this.ball.vx;
                 }
             }
         } else {
-            if (this.p1.x + this.p1.width >= this.ball.x) {
-                var collisionDiff = this.p1.x + this.p1.width - this.ball.x;
+            if (this.paddle1.x + this.paddle1.width >= this.ball.x) {
+                var collisionDiff = this.paddle1.x + this.paddle1.width - this.ball.x;
                 var k = collisionDiff/-this.ball.vx;
                 var y = this.ball.vy*k + (this.ball.y - this.ball.vy);
-                if (y >= this.p1.y && y + this.ball.height <= this.p1.y + this.p1.height) {
+                if (y >= this.paddle1.y && y + this.ball.height <= this.paddle1.y + this.paddle1.height) {
                     // collides with the left paddle
-                    this.ball.x = this.p1.x + this.p1.width;
+                    this.ball.x = this.paddle1.x + this.paddle1.width;
                     this.ball.y = Math.floor(this.ball.y - this.ball.vy + this.ball.vy*k);
                     this.ball.vx = -this.ball.vx;
                 }
@@ -88,30 +116,23 @@ class Game {
             this.ball.vy = -this.ball.vy;
         }
         if (this.ball.x >= this.width)
-            this.score(this.p1);
+            this.score(this.paddle1);
         else if (this.ball.x + this.ball.width <= 0)
-            this.score(this.p2);
+            this.score(this.paddle2);
     }
-    score (p){
-        // player scores
-        p.score++;
-        var player = p == this.p1 ? 0 : 1;
+    score (player_scores){
+        player_scores.score++;
+        var player = player_scores == this.paddle1 ? 0 : 1;
 
         // set ball position
         this.ball.x = this.width/2;
-        this.ball.y = p.y + p.height/2;
+        this.ball.y = player_scores.y + player_scores.height/2;
 
         // set ball velocity
         this.ball.vy = Math.floor(Math.random()*12 - 6);
         this.ball.vx = 7 - Math.abs(this.ball.vy);
-        if (player == 1)
+        if (player_scores == 1)
             this.ball.vx *= -1;
-    }
-    MainLoop(){
-        this.update();
-        this.draw();
-        // Call the main loop again at a frame rate of 30fps
-        setTimeout(this.MainLoop.bind(this), 33.3333);
     }
 }
 
@@ -128,6 +149,22 @@ class Paddle {
         p.fillRect(this.x, this.y, this.width, this.height);
     }
 }
+
+class Ball{
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.vx = 0;
+        this.vy = 0;
+        this.width = 4;
+        this.height = 4;
+    }
+    update(){
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+}
+
 class Display {
     constructor (x, y){
         this.x = x;
@@ -166,23 +203,38 @@ class KeyListener{
     }
 }
 
-class Ball{
-    constructor() {
-        this.x = 0;
-        this.y = 0;
-        this.vx = 0;
-        this.vy = 0;
-        this.width = 4;
-        this.height = 4;
-    }
-        update(){
-            this.x += this.vx;
-            this.y += this.vy;
-        }
-        draw (p){
-            p.fillRect(this.x, this.y, this.width, this.height);
-        }
-
+//AbstractFactory
+class AbstractDraw {
+    constructor(){}
+    clear(){}
+    fill(){}
 }
-// Initialize our game instance
-var game = new Game();
+
+class Draw extends AbstractDraw{
+    constructor(){super();}
+
+    clear(p, x, y, width, height){
+        p.clearRect(x, y, width, height);
+    }
+
+    fill(p, x, y, width, height){
+        p.fillRect(x, y, width, height);
+    }
+}
+
+class DrawBallOrPadle extends AbstractDraw{
+    constructor(){super();}
+    
+    fill(p, x, y, width, height){
+        p.fillRect(x, y, width, height);
+    }
+}
+
+class DrawDisplay extends AbstractDraw {
+    constructor(){super();}
+    fill(p, value, x, y){
+        p.fillText(value, x, y);
+    }
+}
+
+var masina = new GameMechanism();
